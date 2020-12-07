52d4c27f4dfacb03a824b3a8b2a84effbe9eab2a
Mariya Peychinova <mariapeichinova@gmail.com>
2020-12-07 11:49:32 +0000

    make header background and color changable with js

---
+++ js/header.js
@@ -0,0 +1,17 @@
+let header = document.querySelector('header');
+let name = document.querySelector('#name');
+let love = document.querySelector('#love');
+
+    function changeBackground() {
+        header.style.backgroundColor = 'darkslategrey';
+        name.style.color = 'white';
+        love.style.color = 'white';
+    }
+    header.addEventListener('mouseover', changeBackground);
+
+    function backgroundBack() {
+        header.style.backgroundColor = null;
+        name.style.color = null;
+        love.style.color = null;
+    }
+    header.addEventListener('mouseleave', backgroundBack);
