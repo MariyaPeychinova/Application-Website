272b9396cb34ce1b068b9e8e070eb7a42821bdf5
Mariya Peychinova <mariapeichinova@gmail.com>
2020-12-08 19:57:08 +0000

    set animation navLinksFade for the .nav-links li

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,16 @@
 function navSlide() {
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     function openNavLinks() {
+        //Toggle Nav
         nav.classList.toggle('nav-active');
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        })
     }
 console.log(hamburger)
     hamburger.addEventListener('click', openNavLinks);
