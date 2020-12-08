d664ec4667b642bb38fd0d8d3eb0769e691d3755
Mariya Peychinova <mariapeichinova@gmail.com>
2020-12-08 20:03:23 +0000

    set conditional to the animation so works always

--- js/nav.js
+++ js/nav.js
@@ -9,7 +9,11 @@ function navSlide() {
         
         //Animate Links
         navLinks.forEach((link, index) => {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
         })
     }
 console.log(hamburger)
