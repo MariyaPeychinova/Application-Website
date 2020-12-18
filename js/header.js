2c217df9e1818cbf90c01f20d6734dfc0d4949c8
Mariya Peychinova <mariapeichinova@gmail.com>
2020-12-18 21:31:50 +0000

    change let name to const theName

--- js/header.js
+++ js/header.js
@@ -1,17 +1,17 @@
 let header = document.querySelector('header');
-let name = document.querySelector('#name');
+const theName = document.querySelector('#name');
 let love = document.querySelector('#love');
 
     function changeBackground() {
         header.style.backgroundColor = 'darkslategrey';
-        name.style.color = 'white';
+        theName.style.color = 'white';
         love.style.color = 'white';
     }
     header.addEventListener('pointerover', changeBackground);
 
     function backgroundBack() {
         header.style.backgroundColor = null;
-        name.style.color = null;
+        theName.style.color = null;
         love.style.color = null;
     }
     header.addEventListener('pointerleave', backgroundBack);
