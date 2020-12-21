4970d3d61e546d73aefc82b7405522ef54416425
Mariya Peychinova <mariapeichinova@gmail.com>
2020-12-21 10:47:07 +0000

    add animation for when linking .part-1 to page2

--- js/linking.js
+++ js/linking.js
@@ -1,13 +1,14 @@
 definitionList = document.querySelectorAll('.linking');
 linkToPage2 = document.querySelector('.part-1');
 linkToPage3 = document.querySelector('.part-2');//to do
+fadingText = document.querySelectorAll('.text');
 
 definitionList.forEach(function(definitionList){
 definitionList.addEventListener('pointerover', () => {
     definitionList.style.backgroundColor = 'darkslategrey';
     definitionList.style.color = 'white';
     definitionList.classList.toggle('pointer');
-    //definitionList.style.cssText = 'cursor: pointer;';
+   
 });
 
 definitionList.addEventListener('pointerleave', () => {
@@ -17,9 +18,16 @@ definitionList.addEventListener('pointerleave', () => {
 });
 
      
-//definitionList.forEach((definitionList) => {
-    linkToPage2.addEventListener('click', () => {
+
+linkToPage2.addEventListener('click', () => {
+    definitionList.forEach((definitionList) => {
+        definitionList.style.animation = `defListHide 2.5s ease`;
+    });
+    fadingText.forEach((fadingText) => {
+        fadingText.style.animation = `textOpacity 2.5s ease`;
+    });
+    setTimeout(function() {
     window.location.href = 'index2.html';
+    }, 2500);
     //to do for the other pages
 });
-//});
